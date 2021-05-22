a13884c64e84c2aa0697126a7a12a4d2f49b519e
Jeroen de Vrind <devrind@me.com>
2021-05-22 12:39:53 +0200

    Initial Commit

---
+++ Spots/SpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotsApp.swift
+//  Spots
+//
+//  Created by Jeroen de Vrind on 22/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct SpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
